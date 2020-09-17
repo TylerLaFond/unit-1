47358d4910edc86c4350c17aaf5a0469c684eed2
TylerLaFond <816138@mystma.org>
2020-09-17 20:06:56 +0000

    top 10 favriote movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1.star wars movies");
+        System.out.println("2.advengers engame");
+        System.out.println("3.miricle");
+        System.out.println("4.rember the Titans");
+        System.out.println("5.happy gilmore");
+        System.out.println("6.black panther");
+        System.out.println("7.lord of the rings triogy");
+        System.out.println("8.pirates of the caribeien");
+        System.out.println("9.the hobbit");
+        System.out.println("10.Midway");
+
+    }
+}
