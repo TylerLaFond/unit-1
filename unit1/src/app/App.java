628ef9f4e5cf1e9d98a97781b613f79733eb7fec
TylerLaFond <816138@mystma.org>
2020-09-18 20:13:58 +0000

    8 types of code

---
+++ unit1/src/app/App.java
@@ -0,0 +1,32 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      boolean livesInOtsego = false;
+      char firstLetterOfDadsName = 'D';
+      byte numOfPets = 4;
+      short numOfMoneyIHave = 1503;
+      int numOfGamerPointsIHave = 72000;
+      long numOfMoneyWishIHad = 1000000000;
+      float amountOfMoneyCandyBarCost = 4.99f; 
+      double veryLongMathAnswer =8.274950367; 
+
+
+      System.out.println("very long math answer");
+      System.out.println(veryLongMathAnswer);
+      System.out.println("amount of mony a candy bar costs");
+      System.out.println(amountOfMoneyCandyBarCost);
+      System.out.println("Number of moneyt i wish I had");
+      System.out.println(numOfMoneyWishIHad);
+      System.out.println("Number of gamer points i have");
+      System.out.println(numOfGamerPointsIHave);
+      System.out.println("nuber of money i have");
+      System.out.println(numOfMoneyIHave);
+      System.out.println("number of pets I have");
+      System.out.println(numOfPets);
+System.out.println("the first letter of my dads name");
+System.out.println(firstLetterOfDadsName);
+      System.out.println("do i live Otsego");
+     System.out.println(livesInOtsego);
+    }
+}
