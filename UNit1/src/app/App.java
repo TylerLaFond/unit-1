4b7b927dcbef605dd8da8996f97a37439a3cbe3e
TylerLaFond <816138@mystma.org>
2020-09-23 16:37:34 +0000

    mad lib

---
+++ UNit1/src/app/App.java
@@ -0,0 +1,22 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       String person1 = "Tate";
+       String verb1 = "walk";
+       String dayofweek = "Friday";
+       String adjective1 = "blue";
+       String food = "pizza";
+       String verb2 = "run";
+       String place1 = "the effiel tower";
+       String person2 = "Therin";
+       String adjective2 = "cold";
+       String place2 = "spain";
+       String verb3 = "see";
+
+       System.out.println(person1 + "is a programmer who likes to" + verb1);
+       System.out.println("every" +  dayofweek + "they make a" + adjective1 + food + "for breakfeast and" + verb2 + "at" + place1 + "with" + person2);
+       System.out.println("sometimes, they to the" + adjective2 + place2 + "with" person2 + "so they can" + verb3 + "together");
+    
+    }
+}
