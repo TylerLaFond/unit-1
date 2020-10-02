bbb20eab914b2132d30c1b371fbb64e5947b9aa9
TylerLaFond <816138@mystma.org>
2020-10-02 20:14:49 +0000

    e

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,21 +5,17 @@ public class App {
     public static void main(String[] args) throws Exception {
       Scanner input = new Scanner(System.in);
 
-      System.out.println("please enter your name");
-      String FirstName = input.nextLine();
-      System.out.println("please enter your last name");
-      String LastName = input.nextLine();
-      System.out.println("please enter your grade");
-      String Grade = input.nextLine();
-      System.out.println("please enter your age");
-      String Age = input.nextLine();
-      System.out.println("Please enter your favriote color");
-      String FavColor = input.nextLine();
+      int favnumber = 7;
+      
+      if (favnumber == 7){
+          System.out.println(favnumber + "is correct!");
+      }
+      else{
+          System.out.println("incorrect");
+      }
 
-      System.out.println("hello,  " + FirstName + LastName);
-      System.out.println("you are in grade  " + Grade);
-      System.out.println("you are " + Age + "years old");
-      System.out.println("your favriote color is " + FavColor);
+      {System.out.println("all finished");
 
     }
-}
+    
+    String question1 =  
