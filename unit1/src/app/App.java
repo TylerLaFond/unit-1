588a02b8f9502ad0416760eaf5e7f75c46fbfa53
TylerLaFond <816138@mystma.org>
2020-11-13 20:57:39 +0000

    while loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,33 +1,37 @@
 package app;
 
-import java.util.Scanner;
+
 
 public class App {
     
 
     public static void main(String[] args) throws Exception {
+    int count = 0;
+    String color = "red";
+    boolean answer = true;
+
+    while (count < 10) {
+        System.out.println("The count is " + count);
+        count = count + 1;
+    }
     
-        Scanner input = new Scanner(System.in);
+    System.out.println("count finished");
 
-        System.out.println("please enter a noun");
-        String noun1 = input.nextLine();
+    while (color == "red") {
+        System.out.println("The color is " + color);
+        color = "green";
+    }
 
-        System.out.println("please enter a verb");
-        String verb = input.nextLine();
+    System.out.println("Color fimished");
 
-        System.out.println("please enter a noun");
-        String noun2 = input.nextLine();
+    while (answer == true) {
+System.out.println("the answer is " + answer);
+answer = false;
 
-        System.out.println("please enter s adjective");
-        String adjective = input.nextLine();
+    }
+    System.out.println("answer finished");
 
-        System.out.println("Please ente a adverb");
-        String adverb = input.nextLine();
-
-
-
-        System.out.println("there was a " + noun1 + " which was very " +  verb + ". there was a " + noun2 + " next to it which was " + adjective + " than the other. Althought they were both " + adverb);
-        
+      
 
 
       
